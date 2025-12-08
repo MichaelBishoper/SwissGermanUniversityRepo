eb9b91530d81a646b83690b557eb732e47575c66
sevnsecn <arthurmatthewpg@gmail.com>
2025-12-08 09:38:04 +0700

    Fixed navbar, footer and default images on small screens

---
+++ js/navburger.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.querySelector(".nav-toggle");
+    const navList = document.querySelector(".nav-list");
+
+    if (toggle && navList) {
+        toggle.addEventListener("click", function () {
+            navList.classList.toggle("open");
+        });
+    }
+});
